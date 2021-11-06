57f12332815364dbc86bcdfe193ae06eba6f7cb8
boseok jeon <ekfdlf1@naver.com>
2021-11-06 13:18:40 +0900

    project

---
+++ 새 폴더 (4)/phonenumber.js
@@ -0,0 +1,4 @@
+const successmsg=document.querySelector('.successmsg')
+const item_ip = document.querySelector('.item_ip');
+const errormsg = document.querySelector('.errormsg');
+const flag = document.querySelector('#flag');
